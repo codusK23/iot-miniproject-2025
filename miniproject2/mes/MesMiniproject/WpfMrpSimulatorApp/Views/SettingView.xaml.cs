5ede01ca2b874ad3ea205a7f0fae4722e7493f74
codusK23 <codusK23@gmail.com>
2025-06-20 12:22:47 +0900

    commit

--- miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/Views/SettingView.xaml.cs
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/Views/SettingView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,23 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfMrpSimulatorApp.Views
 {
     /// <summary>
-    /// MainView.xaml에 대한 상호 작용 논리
+    /// SettingView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainView : MetroWindow
+    public partial class SettingView : UserControl
     {
-        public MainView()
+        public SettingView()
         {
             InitializeComponent();
-        }
-
-        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            e.Cancel = true;
         }
     }
 }
